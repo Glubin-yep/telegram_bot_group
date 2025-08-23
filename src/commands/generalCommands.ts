f147901ed167f1d6805e1f2defcbe38a5c628f6c
Dmytro Chyr <dima543439@gmail.com>
2025-08-24 00:46:46 +0300

    Add status command

--- src/commands/generalCommands.ts
+++ src/commands/generalCommands.ts
@@ -1,5 +1,7 @@
 import { Telegraf } from "telegraf";
 import { sendMessage } from "../services/messageService";
+import os from "os";
+
 
 export function registerGeneralCommands(bot: Telegraf) {
   bot.command("start", (ctx) => {
@@ -20,5 +22,31 @@ export function registerGeneralCommands(bot: Telegraf) {
   bot.command("source", (ctx) => {
     const text = "https://github.com/Glubin-yep/telegram_bot_group";
     sendMessage(ctx, text);
+  });
+
+
+  bot.command("status", async (ctx) => {
+    const adminId = process.env.ADMIN_ID;
+
+    if (ctx.from?.id.toString() !== adminId) {
+      return sendMessage(ctx, "🚫 Ви не мій господар");
+    }
+
+    const uptime = process.uptime();
+    const uptimeFormatted = new Date(uptime * 1000).toISOString().substr(11, 8);
+
+    const info = [
+      `👤 ID: \`${ctx.from.id}\``,
+      ctx.from.username ? `🔗 Username: @${ctx.from.username}` : "",
+      `💬 Chat ID: \`${ctx.chat?.id}\``,
+      `🖥️ Host: ${os.hostname()}`,
+      `Node.js: \`${process.version}\``,
+      `⏳ Uptime: ${uptimeFormatted}`,
+      `📦 Memory: ${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB`,
+    ]
+      .filter(Boolean)
+      .join("\n");
+
+    await sendMessage(ctx, `🐞 Status інформація:\n\n${info}`);
   });
 }
